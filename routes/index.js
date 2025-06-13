88b4a4e0770bef5b456967291971ec15754664fa
BraxtonGoode <tjbgoode@gmail.com>
2025-06-13 12:25:38 -0700

    copy of sports api

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.use('/', require('./swagger'));
+router.use('/volleyball', require('./volleyball'));
+router.use('/soccer', require('./soccer'));
+
+
+
+// router.use('basketball', require('./basketball'));
+// router.use('/user', require('./user'));
+
+module.exports = router;
